43c5ee77497ae4a54d5ab68ee7c18da268328c70
David Mullins <Mullins6323@gmail.com>
2019-06-25 18:55:51 -0500

    columns setup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 import './Home.scss';
+import NewOrder from '../NewOrder/NewOrder';
+import Inventory from '../Inventory/Inventory';
+import Orders from '../Orders/Orders';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home</h1>
+        <div className="row">
+          <div className="col">
+            <Inventory />
+        </div>
+        <div className="col">
+          <NewOrder />
+          </div><div className="col">
+            <Orders />
+            </div>
+          </div>
       </div>
     );
   }
