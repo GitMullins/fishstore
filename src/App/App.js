a70d5a27f76c74d1ba64c221072e34f3e5164313
David Mullins <Mullins6323@gmail.com>
2019-06-24 19:07:50 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -6,11 +6,20 @@ import Auth from '../components/Auth/Auth';
 import Home from '../components/Home/Home';
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
   render() {
+    const loadComponent = () => {
+      if (this.state.authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
     return (
     <div className="App">
-      <Auth />
-      <Home />
+      {loadComponent()}
     </div>
     );
   }
