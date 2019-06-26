6dcfa64803858ff85fdd2bbd44e59ed0424fbac6
David Mullins <Mullins6323@gmail.com>
2019-06-25 20:51:16 -0500

    printing all inventory

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import fishData from '../../helpers/data/fishData';
+import Fish from '../Fish/Fish';
 
 class Inventory extends React.Component {
   state = {
@@ -9,13 +10,19 @@ class Inventory extends React.Component {
   componentDidMount() {
     fishData.getFishes()
       .then(fishes => this.setState({ fishes }))
-      .catch(err => console.error('could not get fishes'));
+      .catch(err => console.error(err, 'could not get fishes'));
   }
 
   render() {
+    const fishComponents = this.state.fishes.map(fish => (
+      <Fish key={fish.id} fish={fish}/>
+    ));
     return (
       <div className="Inventory">
-        <h3>Inventory</h3>
+        <h2>Inventory</h2>
+        <ul className="fishes">
+          {fishComponents}
+        </ul>
       </div>
     );
   }
---
+++ src/helpers/propz/fishShapes.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const fishShape = PropTypes.shape({
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  status: PropTypes.string.isRequired,
+});
+
+export default { fishShape };
