0e7b82211e4e6dd7a34baf15a39dc7aa91c0c728
David Mullins <Mullins6323@gmail.com>
2019-07-01 21:07:00 -0500

    edit working

--- src/components/NewOrder/NewOrder.js
+++ src/components/NewOrder/NewOrder.js
@@ -9,6 +9,12 @@ class NewOrder extends React.Component {
     orderName: '',
   }
 
+  componentWillUpdate(nextProps) {
+    if (nextProps.orderEditing !== this.props.orderEditing && nextProps.orderEditing.name) {
+      this.setState({ orderName: nextProps.orderEditing.name });
+    }
+  }
+
   nameChange = (e) => {
     e.preventDefault();
     this.setState({ orderName: e.target.value });
@@ -48,7 +54,7 @@ class NewOrder extends React.Component {
   }
 
   render() {
-    const { fishOrder } = this.props;
+    const { fishOrder, orderEditing } = this.props;
     const { orderName } = this.state;
     const orderIds = Object.keys(fishOrder);
     const orderExists = orderIds.length > 0;
@@ -61,7 +67,8 @@ class NewOrder extends React.Component {
 
     return (
       <div className="NewOrder">
-        <h1>New Order</h1>
+        <h1>{Object.keys(orderEditing).length > 1 ? 'Edit Order' : 'New Order'}</h1>
+        {Object.keys(orderEditing).length > 1 ? (<h2>Order id: {orderEditing.id}</h2>) : ''}
         <form className='col-6 offset-3'>
           <div className="form-group">
             <label htmlFor="order-name">Order Name:</label>
